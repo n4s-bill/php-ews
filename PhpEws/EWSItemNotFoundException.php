0e35bd979f3dba622891803e09830bfaaf7caf32
Bill Johnson <bill@number4studios.com>
2016-05-31 15:25:52 -0400

    Create EWSItemNotFoundException.php

---
+++ PhpEws/EWSItemNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Exception class for Exchange Web Services
+ *
+ * @package php-ews
+ * @subpackage Exception
+ */
+ 
+namespace PhpEws;
+
+use EWSException;
+
+/**
+ * Exception class for Exchange Web Services
+ */
+class EWSItemNotFoundException extends EWSException
+{
+}
